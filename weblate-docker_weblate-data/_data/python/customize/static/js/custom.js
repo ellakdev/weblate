87a0163bcbaab91490b3908a90c764aadc5c7d27
GFOSS Developers <webmaster@ellak.gr>
2023-05-30 12:11:09 +0300

    Create custom.js

---
+++ weblate-docker_weblate-data/_data/python/customize/static/js/custom.js
@@ -0,0 +1,8 @@
+
+
+   console.log('loaded!') ;
+
+   document.getElementsByTagName("footer")[0].innerHTML = '<div>  <a href="https://mathe.ellak.gr/" target="_blank">Υλοποίηση με χρήση του Ανοικτού Λογισμικού</a> <a href="https://weblate.org/" target="_blank">Weblate</a> | Άδεια χρήσης περιεχομένου: <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.el">CC-BY-SA</a> |  <a href="https://ellak.gr/stichia-epikinonias-chartis/">Επικοινωνία</a></div>' + document.getElementsByTagName("footer")[0].innerHTML ;
+
+
+
